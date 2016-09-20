4786d6f46e8cd5ecfe01e1c5f60fb851f6c97d21
antonio barquero cuadrado <antonio.barquero@MacBook-pc1219.local>
2016-09-20 09:30:38 +0200

    IPA installer

---
+++ install_ipa.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Install an app in a connected device
+# Prerrequisites --> XCode and ideviceinstaller installed
+if [ $# -ne 1 ]
+	then
+	echo "USAGE: $0 <IPA absolute path>" 
+else	
+	IFS=$'\n'
+	# Get devices UIDS
+	ids=(`xcrun instruments -s devices | cut -d "[" -f2 | cut -d "]" -f1 | tail -n +2`)
+	# Get devices names
+	dev=(`xcrun instruments -s devices | cut -d "(" -f1 | tail -n +2`)
+	correct=0
+	while [ $correct -ne 1 ]; do
+		for (( i = 1 ; i < ${#ids[@]}; i++)) 
+		do
+	        echo "Device [$i]: ${dev[$i]} ${ids[$i]}"
+		done
+
+		echo "Select device"
+		read selection
+		if [ $selection -gt ${#ids[@]} ] 
+			then
+			echo "Invalid option"
+		else
+			echo "Installing IPA..."
+            ideviceinstaller -u ${ids[$selection]} -i $1
+            echo "Done"
+			correct=$((correct + 1))
+		fi
+	done
+fi
